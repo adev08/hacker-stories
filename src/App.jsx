9ee219bf7e82cfac31898526cea4739415aa5fe0
alex mateo <vamateo85@gmail.com>
2023-11-14 18:46:38 -0500

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react';
+
+const title = 'React';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+
+export default App
