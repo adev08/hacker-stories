96fc84112e3efa2fff4c14f59f4df6256ef1a105
alex mateo <vamateo85@gmail.com>
2023-12-02 16:02:46 -0500

    Custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import * as React from 'react';
+import * as React from "react";
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
 
 const App = () => {
   const stories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
     },
     {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem('search') || 'React'
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
-    localStorage.setItem('search', event.target.value);
   };
 
   const searchedStories = stories.filter((story) =>
@@ -59,10 +64,8 @@ const Search = ({ search, onSearch }) => (
 
 const List = ({ list }) => (
   <ul>
-    {list.map(({ objectID, ...item}) => (
-      <Item 
-        key={objectID} {...item}
-      />
+    {list.map(({ objectID, ...item }) => (
+      <Item key={objectID} {...item} />
     ))}
   </ul>
 );
