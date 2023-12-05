2b68ddd7b313a2d08562e1d448de468938a8bcd0
alex mateo <vamateo85@gmail.com>
2023-12-04 19:18:27 -0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', '');
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
